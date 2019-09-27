a2a9e4d7f2481841b5c4a6457ca46def4190c5cc
nabilatulhikma05 <55224785+nabilatulhikma05@users.noreply.github.com>
2019-09-26 18:04:08 -0700

    Add files via upload

---
+++ tugas latihan percabangan.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+	float a,b;
+	int kode;
+	menu :
+	cout << "Kalkulator Sederhana C++ \n";
+	cout << "XRY-cs.id\n\n";
+	cout << "Pilihan Menu : \n";
+	cout << "1.Penjumlahan (+)\n";
+	cout << "2.Pengurangan (-)\n";
+	cout << "3.Perkalian (*)\n";
+	cout << "4.Pembangian (/)\n";
+	cin >> kode;
+	if (kode > 4){
+	cout << "masukkan tidak ada, silahkan masukkan ulang kode.\n";
+	system("pause");
+	system("cls");
+	goto menu ;
+	}
+	else {
+	cout <<"Masukkan angka pertama : ";cin >> a;
+	cout <<"Masukkan angka kedua : ";cin >> b;}
+
+	if (kode == 1){
+		cout << "\nHasil penjumlahan dari "<<a<<" + "<<b<<" adalah : "<<a+b<<endl;
+	}
+	else if (kode == 2){
+		cout << "\nHasil pengurangan dari "<<a<<" - "<<b<<" adalah : "<<a-b<<endl;
+	}
+	else if (kode == 3){
+		cout << "\nHasil perkalian dari "<<a<<" * "<<b<<" adalah : "<<a*b<<endl;
+	}
+	else if (kode == 4){
+		cout << "\nHasil pembagian dari "<<a<<" / "<<b<< " adalah : "<<a/b<<endl; 
+	}
+	return 0;
+}
